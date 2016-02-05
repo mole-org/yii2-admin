409e5dba63fed22962e94d481c32cf553d37bdf0
Jin.Chen <jin.chen@starlight-sms.com>
2016-02-05 11:50:55 +0800

    Update mole-lib.

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -19,5 +19,9 @@ return array(
     'yii\\' => array($vendorDir . '/yiisoft/yii2'),
     'mole\\' => array($vendorDir . '/mole-chen/mole-lib/src'),
     'cebe\\markdown\\' => array($vendorDir . '/cebe/markdown'),
+    'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
+    'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
+    'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
+    'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
     'Faker\\' => array($vendorDir . '/fzaninotto/faker/src/Faker'),
 );
